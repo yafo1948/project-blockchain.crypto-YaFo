7de0fdb1ede1e66f38832df904510930ffaad84c
YaFo <93720099+yafo1948@users.noreply.github.com>
2023-03-18 16:32:47 -0400

    Add files via upload
    completed mutlisig_token.py portion of assignment (10%) to replace the provided skeleton template

--- scripts/multisig_token.py
+++ scripts/multisig_token.py
@@ -1,9 +1,10 @@
 from typing import Tuple
-from eth_keys import KeyAPI
+from eth_keys import KeyAPI # note had issues with eth_keys module in PyCharm
 from eth_keys.backends import NativeECCBackend
 from brownie import RUToken
+from web3 import Web3 # help with hashing
 
-grade_multisig = False # Change this to true if you implemented the multisig token.
+grade_multisig = True # Change this to true if you implemented the multisig token.
 
 keys =  KeyAPI(NativeECCBackend)
 
@@ -25,5 +26,18 @@ def generate_nonce_and_second_signature_transfer2of3(tok: RUToken, sk, multisigA
     key = keys.PrivateKey(bytes.fromhex(sk[2:])) # Can be used with `keys.ecdsa_sign``
 
     # TODO: Implement
-    return (0, Signature(b'\0', b'\0', 0)) # Change this!
+    
+    nonce = tok.nonce(multisigAddr)
 
+    # Structured to prevent replay attacks
+    nonce += 1
+
+    # hashing the message
+    message_hash = Web3.solidityKeccak(['address', 'address', 'address', 'uint256', 'uint256']
+                                       , [tok.address, spender.address, multisigAddr.address, amount, nonce])
+
+    message_signed = keys.ecdsa_sign(message_hash, key)
+    multi_signature = Signature(message_signed.r, message_signed.s, message_signed.v)
+    
+    return (nonce, multi_signature)  # return (0, Signature(b'\0', b'\0', 0)) # Change this!
+    
